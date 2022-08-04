052e74319196aa46cbce00d309df7e187425b18d
Joel <joel.varghese2020@vitbhopal.ac.in>
2022-08-04 17:08:05 +0530

    Commit 2-Added layout and question design

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    Button bn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bn=findViewById(R.id.button);
+        bn.setOnClickListener(new OnClickListener(){
+            @Override
+
+            public void onClick(View v) {
+                TextView t=findViewById(R.id.textInputEditText);
+                String name=t.getText().toString();
+                Bundle extras=new Bundle();
+                Intent intent=new Intent(getApplicationContext(),Q2actActivity.class);
+                extras.putString("name",name);
+                intent.putExtras(extras);
+                startActivity(intent);
+            }
+        });
     }
 }
